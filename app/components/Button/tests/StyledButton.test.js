324c1a3a23d5acb793a3d300a6aeb149494681aa
Subhajit Paul <psubhajit14@gmail.com>
2022-01-07 20:16:18 +0530

    button testcase updated

--- app/components/Button/tests/StyledButton.test.js
+++ app/components/Button/tests/StyledButton.test.js
@@ -3,6 +3,8 @@ import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import StyledButton from '../StyledButton';
 import { VARIANTS } from '../../../utils/constants/variants';
+import { COLORS } from '../../../utils/constants/colorCodes';
+import 'jest-styled-components';
 
 configure({ adapter: new Adapter() });
 
@@ -14,8 +16,19 @@ describe('<StyledButton />', () => {
     return component;
   };
 
-  it('should render with variants', () => {
+  it('should render with primary variants', () => {
     const component = ComponentRenderer(props);
-    expect(component).toBeDefined();
+    expect(component).toHaveStyleRule('color', COLORS.WHITE);
+    expect(component).toHaveStyleRule('background', COLORS.DARK_VIOLET);
+    expect(component).toMatchSnapshot();
+  });
+  it('should render with secondary variants', () => {
+    const component = ComponentRenderer({
+      ...props,
+      variant: VARIANTS.SECONDARY,
+    });
+    expect(component).toHaveStyleRule('color', COLORS.DARK_GREY);
+    expect(component).toHaveStyleRule('background', COLORS.LIGHT_GREY);
+    expect(component).toMatchSnapshot();
   });
 });
--- app/components/Button/tests/index.test.js
+++ app/components/Button/tests/index.test.js
@@ -6,12 +6,15 @@ import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Button from '../index';
+import { VARIANTS } from '../../../utils/constants/variants';
+import StyledButton from '../StyledButton';
 
 configure({ adapter: new Adapter() });
 
 describe('<Button />', () => {
+  const onClick = jest.fn();
   const props = {
-    onButtonClick: jest.fn(),
+    onButtonClick: onClick,
     children: 'Click Me',
   };
 
@@ -23,5 +26,24 @@ describe('<Button />', () => {
   it('should Button renders', () => {
     const component = ComponentRenderer(props);
     expect(component).toBeDefined();
+  });
+  it('should renders primary as default', () => {
+    const component = ComponentRenderer(props);
+    expect(component.find(StyledButton).prop('variant')).toEqual(
+      VARIANTS.PRIMARY,
+    );
+    expect(component.find(StyledButton).prop('onClick')).toEqual(onClick);
+    expect(component.find(StyledButton).text()).toEqual('Click Me');
+    expect(component).toMatchSnapshot();
+  });
+  it('should renders secondary if passed as variant', () => {
+    const component = ComponentRenderer({
+      ...props,
+      variant: VARIANTS.SECONDARY,
+    });
+    expect(component.find(StyledButton).prop('variant')).toEqual(
+      VARIANTS.SECONDARY,
+    );
+    expect(component).toMatchSnapshot();
   });
 });
